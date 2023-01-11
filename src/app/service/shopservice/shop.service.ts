cc498da30535a63657e554f0e917d4ae74062c4b
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2023-01-11 21:50:31 +0700

    fewgritsyuhujirgytytdfsbhnljikuerfgt8dyueshriloy7t8

--- src/app/service/shopservice/shop.service.ts
+++ src/app/service/shopservice/shop.service.ts
@@ -3,6 +3,9 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
 import {Shop} from "../../model/Shop";
+import {Bill} from "../../model/Bill";
+import {BillStatus} from "../../model/BillStatus";
+import {ProductInBillDTO} from "../../model/DTO/ProductInBillDTO";
 const API_URL = `${environment.apiUrl}`;
 
 @Injectable({
@@ -10,35 +13,23 @@ const API_URL = `${environment.apiUrl}`;
 })
 export class ShopService {
 
-  constructor(private http: HttpClient) { }
-  findById(id: number): Observable<Shop> {
-    return this.http.get<Shop>(`${API_URL}/shop/${id}`);
+  constructor(private http:HttpClient) { }
+  getAllBillshop(): Observable<Bill[]>{
+    return this.http.get<Bill[]>(`${API_URL}/order/showBillShop`);
   }
-  findShopById(id: number): Observable<Shop> {
-    return this.http.get<Shop>(`${API_URL}/shop/shop/${id}`);
+
+  getallBillStatus():Observable<BillStatus[]>{
+    return this.http.get<BillStatus[]>(`${API_URL}/order/getallbillstatus`);
   }
-  setIdShop(id : any){
-    localStorage.setItem("idShop",id);
+
+  setbill(idbill:number,idstatus:number):Observable<Bill>{
+    return this.http.get<Bill>(`${API_URL}/order/setbill/${idbill}/${idstatus}`);
   }
-  getIdShop(){
-    return localStorage.getItem("idShop")
+
+  showbillbystatus(id:number):Observable<Bill[]>{
+    return this.http.get<Bill[]>(`${API_URL}/order/showBillShop/${id}`);
   }
-  setImgShop(img : any){
-    localStorage.setItem("imgShop",img);
-  }
-  getImgShop(){
-    localStorage.getItem("imgShop")
-  }
-  setAddressShop(address : any){
-    localStorage.setItem("addressShop", address)
-  }
-  getAddressShop(){
-    return localStorage.getItem("addressShop")
-  }
-  setNameShop(nameShop : any){
-    localStorage.setItem("nameShop", nameShop)
-  }
-  getNameShop(){
-    return localStorage.getItem("nameShop")
+  showbillbyidbill(id:number):Observable<ProductInBillDTO[]>{
+    return this.http.get<ProductInBillDTO[]>(`${API_URL}/order/showBillShopbyidbill/${id}`);
   }
 }
--- src/app/shop/mybillshop/mybillshop.component.ts
+++ src/app/shop/mybillshop/mybillshop.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Bill} from "../../model/Bill";
+import {ProductInBillDTO} from "../../model/DTO/ProductInBillDTO";
+import {ShopService} from "../../service/shopservice/shop.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-mybillshop',
@@ -6,5 +10,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./mybillshop.component.css']
 })
 export class MybillshopComponent {
+  bills :Bill[]=[];
+
+  product:ProductInBillDTO[]=[];
+
+  product1!:any;
+
+
+  constructor(private showbillshop: ShopService ,private router:Router) {
+  }
+
+  ngOnInit() {
+    this.showbillshop1
+  }
+
+  showbillshop1():void{
+    this.showbillshop.getAllBillshop().subscribe((data) => {
+      this.bills = data
+    })
+  }
+
+  setbillshop(idstatus:number,idbill:number):void {
+    this.showbillshop.setbill(idbill, idstatus).subscribe((data) => {
+      this.showbillshop1()
+    })
+    this.router.navigate(["/bill"])
+  }
+
+  showbillbyidbill(id:number):void{
+    this.showbillshop.showbillbyidbill(id).subscribe((data) => {
+      this.product = data
+      console.log(this.product);
+      let str = ""
+      let str1=`<h4>Thông tin đơn hàng :</h4>`
+
+      for (let i = 0; i < this.product.length; i++) {
+        str += `
+                    <table style="margin-left: 50px">
+                      <hr>
+                      <tr>
+                      <th>Sản phẩm :</th>
+</tr>
+                      <tr  >
+                        <td>Tên : ${this.product[i].name} </td>
+
+                        <td>Ảnh sp : <img src="${this.product[i].img} " style="width: 100px;height: 50px" alt=""></td>
+                                                <td> </td>
+
+                        <td>Giá : ${this.product[i].price} </td>
+                    </tr>
+                    </table>
+                    <hr>
+
+`
+      }
+      // @ts-ignore
+      document.getElementById(id).innerHTML = str;
+      // @ts-ignore
+      document.getElementById("thongtin"+id).innerHTML = str1;
+    })
+
+  }
 
 }
