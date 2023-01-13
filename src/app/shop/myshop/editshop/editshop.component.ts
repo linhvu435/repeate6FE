0d10213f925cc7fdc855f7ed88cb1d3140be939c
sangpk <dinhsangcnc@gmail.com>
2023-01-13 16:29:21 +0700

    commit2

---
+++ src/app/shop/myshop/editshop/editshop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editshop',
+  templateUrl: './editshop.component.html',
+  styleUrls: ['./editshop.component.css']
+})
+export class EditshopComponent {
+
+}
